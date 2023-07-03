ee66ee1edcdf4f3f174fbdf00121bf0fa4c9cbf2
Tunisia Artope <tartope@xpanxion.com>
2023-07-03 13:38:10 -0400

    Add anagrams and compress solutions.

--- algos-easy/anagrams.py
+++ algos-easy/anagrams.py
@@ -5,17 +5,40 @@
 # Anagrams are strings that contain the same characters, but in any order.
 
 def anagrams(s1, s2):
-  pass #TODO:
+    
+  # First, check for strings lengths
+  if len(s1) != len(s2): return False
 
+  # Initialize a dict to hold the letters present in first string
+  count = {}
+  for char in s1:
+    if char not in count:
+      count[char] = 1
+    else:
+      count[char] +=1
 
+  # Loop through second dictionary 
+  for char in s2:
+    # If char is not in dict
+    if char not in count:
+      # Return False
+      return False
+    else:
+      # Else if char is in dict, decrement the value
+      count[char] -= 1
+      # If the value is negative, return False
+      if count[char] < 0:
+        return False
+  # After the for loop is done, return True
+  return True    
 
 
 
 
 
 # # TEST CASES
-anagrams('restful', 'fluster') # -> True
-# anagrams('cats', 'tocs') # -> False
+# print(anagrams('restful', 'fluster')) # -> True
+# print(anagrams('cats', 'tocs')) # -> False
 # anagrams('monkeyswrite', 'newyorktimes') # -> True
 # anagrams('paper', 'reapa') # -> False
 # anagrams('elbow', 'below') # -> True
--- algos-easy/compress.py
+++ algos-easy/compress.py
@@ -11,16 +11,43 @@
 
 # You can assume that the input only contains alphabetic characters.
 
+# Two pointer solution:
+# i = 0 -> end (tracks the letter)
+# j = 0 -> end (tracks the occurances)
 
 def compress(s):
-    pass # TODO:
+    s += '.'
+    result = []
+    i, j = 0, 0
+
+    while j < len(s):
+        if s[i] == s[j]:
+            j +=1
+        else: 
+            letter_count = j - i
+            if letter_count == 1:
+                result.append(s[i])
+            else:
+                result.append(str(letter_count))
+                result.append(s[i])
+            # Catch i up to j
+            i = j        
+    return ''.join(result)
 
 
 
+# ltr_count = 
+# 0 1 2 3 4 5 6 7 8 9
+# c c a a a t s s s .
+# i
+# j
+
+# [    ]
 
 # TEST CASES
-compress('ccaaatsss') # -> '2c3at3s'
-# compress('ssssbbz') # -> '4s2bz'
-# compress('ppoppppp') # -> '2po5p'
-# compress('nnneeeeeeeeeeeezz') # -> '3n12e2z'
-# compress('yyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyy');  # -> '127y'
+print(compress('ccaaatsss')) # -> '2c3at3s'
+print(compress('ssssbbz')) # -> '4s2bz'
+print(compress('ppoppppp')) # -> '2po5p'
+print(compress('nnneeeeeeeeeeeezz')) # -> '3n12e2z'
+print(compress('yyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyy'));  # -> '127y'
+
